d8c12b37aa3259f1f3168bd42f98cc606f3b9217
Leonardo Mendes <leonardo_mendes2002@hotmail.com>
2025-10-04 09:13:47 -0300

    stringConnection para o banco de dados

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using ApiCatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
